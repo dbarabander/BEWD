fc32f022eaf810338ca78b96153f882a269a20e8
Bernard J Kravitz <bernie@gowatchit.com>
2014-06-25 19:30:22 -0400

    Revert "Submitting homework for Secret Number"

--- BEWD_NYC_11_Homework/_Scott_Harris/Secret_Number/Secret_Number_Scott_Harris.rb
+++
@@ -1,9 +0,0 @@
-puts "What number am I thinking of?"
-guess = gets
-
-if guess == 42
-	puts "Correct! You've discovered the meaning of life."
-else guess != 42
-	puts "WRONG. YOU HAVE LAUCHED ALL OF THE WORLD'S NUCLEAR WEAPONS. CONGRATS."
-end
-
