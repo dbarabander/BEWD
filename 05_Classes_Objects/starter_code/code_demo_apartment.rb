8a0458daa0740fc3430aaf67b4c44ccbdff1278a
BJK <bernie@gowatchit.com>
2014-07-07 08:06:24 -0400

    added 'sample' code from class demo

--- 05_Classes_Objects/starter_code/code_demo_apartment.rb
+++ 05_Classes_Objects/starter_code/code_demo_apartment.rb
@@ -1,7 +1,32 @@
-#Explaining classes, objects, attr_accessor & load paths. 
-#TIME: 30 min
-
-
 class Apartment
+  attr_accessor :name, :rent, :sqft, :num_bedrooms, :num_bathrooms, :renters
 
+  def initialize(name, sqft, num_bedrooms, num_bathrooms)
+    @name = name
+    @rent = 0
+    @sqft = sqft
+    @num_bedrooms = num_bedrooms
+    @num_bathrooms = num_bathrooms
+    @renters = []
+  end
+
+  def is_occupied?
+    @renters.any?
+  end
+
+  def to_s
+    "#{@name} has #{@sqft} sqft and #{@renters.count} renters."
+  end
+end
+
+my_apartment = Apartment.new("Viaud Brooklyn Residential", 800, 1, 1)
+#Ask students how they would add renters to my_apartment.
+#my_apartment.renters = ['Peter', 'Andre']
+#OR: my_apartment.renters << 'Gracie'
+
+if my_apartment.is_occupied?
+  puts "This apartment is not for rent"
+else
+  puts my_apartment
+  puts "This apartment is for rent, ask your broker for more details"
 end
