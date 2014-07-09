e5980309f9d7e01c8cb6b55572d96efa549d908a
BJK <bernie@gowatchit.com>
2014-07-09 17:58:48 -0400

    filled in demo for inheritance

--- 06_Sharing_Behavior/starter_code/code_demo_stdnt_inheritance.rb
+++ 06_Sharing_Behavior/starter_code/code_demo_stdnt_inheritance.rb
@@ -4,21 +4,53 @@
 
 # Create an employee class.
 class Employee
+  attr_accessor :first_name, :last_name
 
+  def initialize(first_name, last_name)
+    @first_name=first_name
+    @last_name=last_name
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
 
 end
 
 # A manager is contactable by email,
 # and wants their title as part of their name display
 class Manager < Employee
+  attr_accessor :title, :email
+  
+  def initialize(first_name, last_name, title, email)
+    super(first_name, last_name)
+    @title=title
+    @email=email
+  end
 
+  def full_name
+    "#{super} (#{@title})"
+  end
+
+  def contact
+    @email
+  end
 
 end
 
 # A marketer can be contacted via twitter
 # but is fine with the default name display
 class Marketer < Employee
+  attr_accessor :twitter_handle
 
+  def initialize(first_name, last_name, twitter_handle)
+    super(first_name, last_name)
+    @twitter_handle=twitter_handle
+  end
+
+  def contact
+    @twitter_handle
+  end
 
 end
 
