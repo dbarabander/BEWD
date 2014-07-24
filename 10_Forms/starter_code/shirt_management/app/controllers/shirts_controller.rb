3a8328ee9dfab1acacca1f68af7dd72109e0e71e
BJK <bernie@gowatchit.com>
2014-07-23 20:04:47 -0400

    finishing shirts app

--- 10_Forms/starter_code/shirt_management/app/controllers/shirts_controller.rb
+++ 10_Forms/starter_code/shirt_management/app/controllers/shirts_controller.rb
@@ -4,6 +4,40 @@ class ShirtsController < ApplicationController
   end
 
   def show
+    load_shirt
+  end
+
+  def new
+    @shirt = Shirt.new
+  end
+
+  def create
+    @shirt = Shirt.new(safe_shirt_params)
+    if @shirt.save
+      redirect_to shirt_path(@shirt)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    load_shirt
+  end
+
+  def update
+    load_shirt
+    @shirt.update safe_shirt_params
+
+    redirect_to shirt_path(@shirt)
+  end
+
+  private 
+  def safe_shirt_params
+    params.require('shirt').permit(:name, :description, :image)
+  end
+
+  def load_shirt
     @shirt = Shirt.find_by id: params[:id]
   end
 end
+
