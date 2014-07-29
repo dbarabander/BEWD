6ea81a4b9b8d8c811c5a5e84f038e5a197c2c1d0
BJK <bernie@gowatchit.com>
2014-07-28 20:19:14 -0400

    added auth to app

--- 12_authentication/starter_code/rewlsy/app/controllers/stories_controller.rb
+++ 12_authentication/starter_code/rewlsy/app/controllers/stories_controller.rb
@@ -1,4 +1,5 @@
 class StoriesController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create]
   def index
     @stories = Story.search(params[:q])
   end
--- 12_authentication/starter_code/rewlsy/db/schema.rb
+++ 12_authentication/starter_code/rewlsy/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140728124821) do
+ActiveRecord::Schema.define(version: 20140728231446) do
 
   create_table "stories", force: true do |t|
     t.string   "title"
     t.text     "link"
     t.integer  "upvotes"
     t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
