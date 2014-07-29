a0b86cb90c8d631eccfa9981c01ef90d171a2465
BJK <bernie@gowatchit.com>
2014-07-28 20:25:25 -0400

    added logout functionality

--- 12_authentication/starter_code/rewlsy/app/controllers/sessions_controller.rb
+++ 12_authentication/starter_code/rewlsy/app/controllers/sessions_controller.rb
@@ -17,5 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
+    redirect_to stories_path
   end
 end
--- 12_authentication/starter_code/rewlsy/config/routes.rb
+++ 12_authentication/starter_code/rewlsy/config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'stories#index'
 
   resources :stories
-  resources :sessions
+  resources :sessions, only: [:new, :create]
+  get '/logout', to: "sessions#destroy", as: 'logout'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
